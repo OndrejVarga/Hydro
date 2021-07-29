725d469a607b603a93d1cb818fa8837c67c454b1
Ondrej Varga <ondrejvarga.varga@gmail.com>
2021-07-29 17:28:38 +0100

    release-ready

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Hydro',
+      title: 'Water Reminder',
       theme: MyTheme.getTheme(context),
       home: MultiProvider(
           providers: [ChangeNotifierProvider.value(value: Core())],
